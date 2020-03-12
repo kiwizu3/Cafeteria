b9ba500eada94a821f07aa7bacb79e11dab7da3f
DenEdwards <denzelsedwards@gmail.com>
2020-03-12 13:17:53 -0400

    Update to Contacts

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { FaPhone } from "react-icons/fa";
 export default function Title({ title }) {
 	return (
 		<div className="contact-title">
 			<h4>{title}</h4>
-			<FaPhone />
 			<div />
 		</div>
 	);
