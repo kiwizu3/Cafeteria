f10104b5cdc3fc59514841b1db22d24a5422a088
aleezah <aleeza97@hotmail.com>
2020-03-18 20:03:14 -0400

    checkout page
    need database to complete

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,16 +1,49 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import employee from "../images/Employee.png";
+import './Checkout.css';
 
 export default function Checkout() {
 	return (
 		<>
 			<Navbar />
-			<Hero>
-				<a>
+			<body>
+			<Hero  hero="landHeroBk">
+				<div className="titleBar w-100 block">
+					<h1 className="center-text">Order Summary</h1>
+				</div>
+				<div className = "block white center-text">
+					<span className="bold">Order #1</span>
+					<div >
+						<span>1</span><span className="priceSpacing"> First Order #1</span><span>Price</span>
+					</div>
+					<hr></hr>
+					<div >
+						<span className="subSpace"> SUBTOTAL</span><span>Price</span>
+					</div>
+					<div >
+						<span className="taxSpace"> TAX</span><span>Price</span>
+					</div>
+					<div >
+						<span className="totalSpace"> TOTAL</span><span>Price</span>
+					</div>
+					<a>
 					<button>Checkout Order</button>
-				</a>
+					</a>
+
+					
+
+
+	
+
+
+
+
+				</div>
 			</Hero>
+			</body>
+
 		</>
 	);
 }
